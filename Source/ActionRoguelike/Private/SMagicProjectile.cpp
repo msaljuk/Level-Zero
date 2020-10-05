157c32d96d07ad873f55bbecf7707c5eb444abb4
Saljuk Gondal <62533326+msaljuk@users.noreply.github.com>
2020-10-05 08:16:40 +0500

    Lecture 8 completed

--- Source/ActionRoguelike/Private/SMagicProjectile.cpp
+++ Source/ActionRoguelike/Private/SMagicProjectile.cpp
@@ -45,7 +45,7 @@ void ASMagicProjectile::Tick(float DeltaTime)
 
 void ASMagicProjectile::OnActorOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (OtherActor)
+	if (OtherActor && OtherActor != GetInstigator())
 	{
 		USAtttributeComponent* AttributeComp = Cast<USAtttributeComponent>(OtherActor->GetComponentByClass(USAtttributeComponent::StaticClass()));
 
---
+++ Source/ActionRoguelike/Public/STargetDummy.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "STargetDummy.generated.h"
+
+class UStaticMeshComponent;
+class USAtttributeComponent;
+
+UCLASS()
+class ACTIONROGUELIKE_API ASTargetDummy : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASTargetDummy();
+
+protected:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UStaticMeshComponent* MeshComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	USAtttributeComponent* AttributeComp;
+
+	UFUNCTION()
+	void OnHealthChanged(AActor* InstigatorActor, USAtttributeComponent* OwningComp, float NewHealth, float Delta);
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
